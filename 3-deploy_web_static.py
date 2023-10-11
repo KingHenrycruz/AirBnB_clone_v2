360b2fd5540589d98e1aea025f29e0c4ff699fa0
KingHenrycruz <KingHenrycruz@gmail.com>
2023-10-11 01:23:46 +0000

    Updated server hosts

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['35.153.226.71', '54.174.70.122']
 
 
 @runs_once
