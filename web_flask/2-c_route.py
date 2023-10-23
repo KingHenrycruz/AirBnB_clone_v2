d2104e884a5c1a4b2bca47dd7f154de20141fa3e
KingHenrycruz <KingHenrycruz@gmail.com>
2023-10-23 03:53:29 +0000

    final commit ++ advanced

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,14 +9,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """returns HBNB"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
+    """ display "C " followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
