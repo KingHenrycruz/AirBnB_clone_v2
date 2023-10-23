b6b05d4b9bcd5b918e1cecca95d4f92bdcf9ecc8
KingHenrycruz <KingHenrycruz@gmail.com>
2023-10-23 03:07:52 +0000

    initial commit made executable

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -24,5 +24,14 @@ def cisfun(text):
 def pythoniscool(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbersandtemplates(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
